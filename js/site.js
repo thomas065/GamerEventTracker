313ad1b9ab7932e7f28640143119e01084b17e5e
Thomas Bell <thomasjbell065@gmail.com>
2023-10-19 12:02:17 -0400

    updated code

--- js/site.js
+++ js/site.js
@@ -3,15 +3,13 @@ function displayMessage() {
     let msg = document.getElementById('message').value;
 
     // alert('Hello Guardian Cohort!')
-    Swal.fire(
-        {
-            icon: 'success',
-            backdrop: false,
-            title: '<strong>APP NAME</strong>',
-            text: msg, // <---- replace this text with that variable
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-        }
-    );
+    Swal.fire({
+        icon: 'success',
+        backdrop: false,
+        title: '<strong>PAX Task Event Tracker</strong>',
+        text: msg, // <---- replace this text with that variable
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+    });
 }
